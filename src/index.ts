b8afe2ea260859c8ce1bdcf53f3f262df02b95ed
Fati Chen <chen@lirmm.fr>
2019-09-29 18:10:37 +0200

    prepare for release :)

--- src/index.ts
+++ src/index.ts
@@ -98,7 +98,7 @@ export const diamondGraphRotation: Function<Props> = function(
   // transform to js constraint
   const tmodel = ReformatLP(lpsolve);
 
-  console.log(lpsolve);
+  // console.log(lpsolve);
   const solver = Solve(tmodel);
 
   const { feasible, result, bounded, ...rest } = solver;
@@ -132,20 +132,20 @@ export const diamondGraphRotation: Function<Props> = function(
     ...rotatedPos[index]
   }));
 
-  console.log(JSON.stringify(graph));
-  console.log(JSON.stringify(diamonds));
-  console.log(
-    JSON.stringify(
-      diamonds.map(({ wii: width, height, ...d }) => {
-        const update: any = {};
-        if (positions[d.index]) {
-          if (positions[d.index].x) update.x = positions[d.index].x;
-          if (positions[d.index].y) update.y = positions[d.index].y;
-        }
-        return { ...d, ...update, width: width * 2, height: height * 2 };
-      })
-    )
-  );
+  // console.log(JSON.stringify(graph));
+  // console.log(JSON.stringify(diamonds));
+  // console.log(
+  //   JSON.stringify(
+  //     diamonds.map(({ wii: width, height, ...d }) => {
+  //       const update: any = {};
+  //       if (positions[d.index]) {
+  //         if (positions[d.index].x) update.x = positions[d.index].x;
+  //         if (positions[d.index].y) update.y = positions[d.index].y;
+  //       }
+  //       return { ...d, ...update, width: width * 2, height: height * 2 };
+  //     })
+  //   )
+  // );
   return { graph: { nodes: updatedNodes, edges: graph.edges } };
 };
 
@@ -165,7 +165,6 @@ function node2Diamond(
   h: number
 ): Diamond {
   return {
-    up: { kind: 'diamond' },
     index,
     x,
     y,
