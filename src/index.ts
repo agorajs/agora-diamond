2f321bc7cc49cc3178c4fa28127487b3c35ed54a
Fati Chen <chen@lirmm.fr>
2019-09-29 15:53:23 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Function, Node, toPolar, toCartesian } from 'agora-graph';
+import { Function, Node, toPolar, toCartesian, round } from 'agora-graph';
 import _ from 'lodash';
 import { Solve, ReformatLP } from 'javascript-lp-solver';
 
@@ -18,59 +18,132 @@ export const diamondGraphRotation: Function<Props> = function(
   graph,
   options = { padding: 0 }
 ) {
-  const vs = _.sortBy(graph.nodes, ({ x }) => x);
-  const hs = _.sortBy(graph.nodes, ({ y }) => y);
-  const diamonds = _.map(graph.nodes, n => {
+  graph.nodes.sort((a, b) => a.index - b.index);
+
+  const rotatedNodes = _.map(graph.nodes, n => {
     const polar = toPolar(n);
     polar.theta += Math.PI / 4;
     const cart = toCartesian(polar);
+
+    return { ...n, ...cart };
+  });
+  const vs = _.sortBy(rotatedNodes, ({ x }) => x);
+  const hs = _.sortBy(rotatedNodes, ({ y }) => y);
+  const diamonds = _.map(rotatedNodes, n => {
     return node2Diamond(
-      { ...n, ...cart },
+      n,
       _.findIndex(vs, ['index', n.index])!,
       _.findIndex(hs, ['index', n.index])!
     );
   });
 
-  console.log(diamonds);
-  console.log(hs);
+  const constraints: string[] = [];
 
-  const constraints: { [k: string]: any } = {};
-  const temp = _.map(diamonds, n => {
-    // y'_v(i) <= y'_v(i+1)
-    const building: { [k: string]: any } = {};
-    if (n.v < vs.length - 1) {
-      const yConstraint = `y'_${n.index} <= y'_${vs[n.v + 1].index}`;
-      building[yConstraint] = 1;
-      constraints[yConstraint] = 1;
+  const minimize = _(diamonds)
+    .map(({ index, x, y }) => `x${index} - ${x} + y${index} - ${y}`)
+    .join(' + ');
+
+  constraints.push('min: ' + minimize + ';');
+
+  diamonds.sort((a, b) => a.index - b.index);
+
+  for (let i = 0; i < diamonds.length; i++) {
+    const { index, v, h } = diamonds[i];
+    if (v < vs.length - 1) {
+      constraints.push(`x${index} - x${vs[v + 1].index} <= 0;`);
     }
     // x'_v(i) <= y'_v(i+1)
-    if (n.h < hs.length - 1) {
-      const xConstraint = `x'_${n.index} <= x'_${hs[n.h + 1].index}`;
-      building[xConstraint] = 1;
-      constraints[xConstraint] = { equal: 1 };
+    if (h < hs.length - 1) {
+      constraints.push(`y${index} - y${hs[h + 1].index} <= 0;`);
     }
+  }
 
-    return building;
-  });
+  //
+  // for (let i = 0; i < diamonds.length; i++) {
+  //   const { index: iIdx, y: yi } = diamonds[i];
+  //   let ystar = -1;
+  //   for (let j = i - 1; j > 0; j--) {
+  //     const { index: jIdx, y: yj } = diamonds[j];
 
-  const tmodel = ReformatLP(`min: x1 + x2;/*comment*/
-x1 >= 1;
-x2 >= 1;
-x1 + x2 >= 2;
-int x1;`);
+  //     if (ystar < yj && yj <= yi) {
+  //       constraints.push(`y${jIdx} - y${iIdx} <= 0;`);
+  //       ystar = yj;
+  //     }
+  //   }
+  // }
 
-  console.log(tmodel);
+  // forall 1<=i<=j<=n xi <= xj and yi <= yj
+  //    x'j - x'i + y'j - y'i >= wi +wj
+  // console.log(diamonds);
 
-  const model = {
-    optimize: {},
-    constraints: {},
-    variables: { ...diamonds }
-  };
+  for (let i = 0; i < diamonds.length; i++) {
+    const { index: iIdx, x: xi, y: yi, width: wi } = diamonds[i];
+    for (let j = i + 1; j < diamonds.length; j++) {
+      const { index: jIdx, x: xj, y: yj, width: wj } = diamonds[j];
+      if (xi <= xj && yi <= yj) {
+        constraints.push(
+          `x${jIdx} - x${iIdx} + y${jIdx} - y${iIdx} >= ${(wi + wj) *
+            Math.SQRT2};`
+        );
+      }
+      if (xi >= xj && yi <= yj) {
+        constraints.push(
+          `x${iIdx} - x${jIdx} + y${jIdx} - y${iIdx} >= ${(wi + wj) *
+            Math.SQRT2};`
+        );
+      }
+      if (xi >= xj && yi >= yj) {
+        constraints.push(
+          `x${iIdx} - x${jIdx} + y${iIdx} - y${jIdx} >= ${(wi + wj) *
+            Math.SQRT2};`
+        );
+      }
+      if (xi <= xj && yi >= yj) {
+        constraints.push(
+          `x${jIdx} - x${iIdx} + y${iIdx} - y${jIdx} >= ${(wi + wj) *
+            Math.SQRT2};`
+        );
+      }
+    }
+  }
 
-  console.log(model);
+  for (let index = 0; index < diamonds.length; index++) {
+    const { index: i, x, y } = diamonds[index];
+    constraints.push(`x${i} >= ${x};`);
+    constraints.push(`y${i} >= ${y};`);
+  }
 
-  //   const solver = Solve();
+  console.log(constraints.join('\n'));
 
+  const tmodel = ReformatLP(constraints.join('\n'));
+
+  // console.log(tmodel);
+  const solver = Solve(tmodel);
+  const { feasible, result, bounded, ...rest } = solver;
+  const positions: { [k: string]: any } = _.transform(
+    rest,
+    function(result: any, val: number, key: string) {
+      const tpe = key.substr(0, 1);
+      const index = key.substr(1);
+      (result[index] || (result[index] = {}))[tpe] = val;
+    },
+    {}
+  );
+
+  console.log(JSON.stringify(graph));
+  console.log(JSON.stringify(diamonds));
+  console.log(
+    JSON.stringify(
+      diamonds.map(({ width, height, ...d }) => {
+        const update: any = {};
+        if (positions[d.index]) {
+          if (positions[d.index].x) update.x = positions[d.index].x;
+          if (positions[d.index].y) update.y = positions[d.index].y;
+        }
+        return { ...d, ...update, width: width * 2, height: height * 2 };
+      })
+    )
+  );
   return { graph };
 };
 
@@ -91,13 +164,14 @@ function node2Diamond(
   h: number
 ): Diamond {
   return {
+    up: { kind: 'diamond' },
     index,
     x,
     y,
     v,
     h,
-    width: Math.max(height, width),
-    height: Math.max(height, width)
+    width: Math.max(height, width) / 2,
+    height: Math.max(height, width) / 2
   };
 }
 
--- src/index.unit.test.ts
+++ src/index.unit.test.ts
@@ -1,32 +1,54 @@
 import { diamondGraphRotation } from '.';
 const graph = {
-  nodes: [
-    {
-      index: 0,
-      x: 15,
-      y: 10,
-      label: '0',
-      width: 10,
-      height: 10
-    },
-    {
-      index: 1,
-      x: 10,
-      y: 20,
-      label: '0',
-      width: 10,
-      height: 10
-    },
-    {
-      index: 3,
-      x: 20,
-      y: 20,
-      label: '0',
-      width: 10,
-      height: 10
-    }
+  edges: [
+    { source: 0, target: 1 },
+    { source: 0, target: 2 },
+    { source: 0, target: 3 },
+    { source: 0, target: 4 },
+    { source: 0, target: 5 },
+    { source: 1, target: 2 },
+    { source: 1, target: 3 },
+    { source: 1, target: 4 },
+    { source: 1, target: 5 },
+    { source: 2, target: 3 },
+    { source: 2, target: 4 },
+    { source: 2, target: 5 },
+    { source: 3, target: 4 },
+    { source: 3, target: 5 },
+    { source: 4, target: 5 },
+    { source: 2, target: 6 },
+    { source: 1, target: 6 },
+    { source: 5, target: 6 },
+    { source: 3, target: 6 },
+    { source: 4, target: 6 },
+    { source: 2, target: 7 },
+    { source: 3, target: 7 },
+    { source: 4, target: 7 },
+    { source: 6, target: 7 },
+    { source: 1, target: 7 },
+    { source: 4, target: 8 },
+    { source: 3, target: 8 },
+    { source: 1, target: 8 },
+    { source: 5, target: 8 },
+    { source: 7, target: 8 },
+    { source: 5, target: 9 },
+    { source: 4, target: 9 },
+    { source: 3, target: 9 },
+    { source: 6, target: 9 },
+    { source: 1, target: 9 }
   ],
-  edges: []
+  nodes: [
+    { index: 0, label: 'undefined', x: 87, y: 44, width: 20, height: 10 },
+    { index: 1, label: 'undefined', x: 57, y: 43, width: 20, height: 10 },
+    { index: 2, label: 'undefined', x: 67, y: 72, width: 20, height: 10 },
+    { index: 3, label: 'undefined', x: 46, y: 58, width: 20, height: 10 },
+    { index: 4, label: 'undefined', x: 47, y: 46, width: 20, height: 10 },
+    { index: 5, label: 'undefined', x: 61, y: 29, width: 20, height: 10 },
+    { index: 6, label: 'undefined', x: 25, y: 57, width: 20, height: 10 },
+    { index: 7, label: 'undefined', x: 45, y: 85, width: 20, height: 10 },
+    { index: 8, label: 'undefined', x: 80, y: 67, width: 20, height: 10 },
+    { index: 9, label: 'undefined', x: 25, y: 25, width: 20, height: 10 }
+  ]
 };
 
 test('Agora Diamond', () => {
